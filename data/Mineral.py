92acd268b4c0480c5e429ee31289de9d9edb08db
Patrice Boissier <boissier@ipgp.fr>
2015-10-26 16:33:25 +0400

    Added data package and Mineral class skeleton

---
+++ data/Mineral.py
@@ -0,0 +1,27 @@
+'''
+Created on 26 oct. 2015
+
+:copyright:
+    Guillaume Boudoire (guillaume.boudoire@gmail.com)
+        Laboratoire GeoSciences Réunion
+        Université de La Reunion
+        Observatoire Volcanologique du Piton de La Fournaise
+        Institut de Physique du Globe de Paris
+    Patrice Boissier (boissier@ipgp.fr)
+        Observatoire Volcanologique du Piton de La Fournaise
+        Institut de Physique du Globe de Paris
+:license:
+    GNU Lesser General Public License, Version 3
+    (http://www.gnu.org/copyleft/lesser.html)
+'''
+
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
